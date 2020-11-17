29e789833f0644f624fbab3215b8c71ee9d8699d
shibaev.aleksey <shibaev.aleksey@otr.ru>
2020-11-17 17:02:12 +0300

    2

--- tempId/src/main/java/MyRunnable.java
+++ tempId/src/main/java/MyRunnable.java
@@ -1,8 +1,12 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import static java.lang.Thread.sleep;
 
 public class MyRunnable implements Runnable {
 
-	private static final String URL = "testUrl";
+	private static final String URL = "https://www.google.com/";
 
 	public void run() {
 		try {
@@ -13,7 +17,11 @@ public class MyRunnable implements Runnable {
 	}
 
 	private void doLogic() throws Exception {
-		System.out.println("logic");
+		Connection connect = Jsoup.connect(URL);
+		Document document = connect.get();
+
+		//todo
+
 		sleep(1000);
 	}
 }

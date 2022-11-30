6edb77bd8866825bc66c8e860450a13c6e70a54a
shibaev.aleksey <shibaev.aleksey@otr.ru>
2022-11-30 21:39:30 +0300

    rest api spring boot test +

--- src/main/java/com/company/app/controllers/MainController.java
+++ src/main/java/com/company/app/controllers/MainController.java
@@ -2,9 +2,7 @@ package com.company.app.controllers;
 
 import com.company.app.services.api.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -17,5 +15,10 @@ public class MainController {
 	@PostMapping(value = "/")
 	public void interceptAll(@RequestBody Map<String, Object> lookupRequestObject) {
 		notificationService.eventNotification(lookupRequestObject.toString());
+	}
+
+	@GetMapping(value = "/bot", produces = "application/json")
+	public void writeBot(@RequestParam(required = true) String message) {
+		notificationService.eventNotification(message);
 	}
 }
--- src/test/java/com/company/app/ApplicationTest.java
+++ src/test/java/com/company/app/ApplicationTest.java
@@ -3,15 +3,28 @@ package com.company.app;
 import com.company.app.services.api.NotificationService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 public class ApplicationTest extends AbstractTest {
 
+	@LocalServerPort
+	private Integer port;
+
 	@Autowired
 	private NotificationService notificationService;
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	public void smokeTest() {
-		notificationService.eventNotification("Тестовое приложение поднялось.");
+		notificationService.eventNotification("1. Тестовое приложение поднялось.");
 		System.out.println("Тестовое приложение поднялось.");
+	}
+
+	@Test
+	public void accessApplication() {
+		String message = String.format("2. На порту %s успешно поднялось тестовое приложение.", port);
+		testRestTemplate.getForEntity(String.format("/bot?message=%s", message), Object.class);
 	}
 }

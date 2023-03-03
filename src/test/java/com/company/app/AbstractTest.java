f2820d38ce0f51453d434c9f81241c36e0b33b1a
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-03-04 00:21:43 +0300

    шедулер не нужен в спринг тест

--- src/test/java/com/company/app/AbstractTest.java
+++ src/test/java/com/company/app/AbstractTest.java
@@ -1,10 +1,13 @@
 package com.company.app;
 
+import com.company.app.config.SchedulerConfig;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@MockBean(SchedulerConfig.class)
 public abstract class AbstractTest {
 }

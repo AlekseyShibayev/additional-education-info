fb532068fc18a3655bfd82bcd600c8fe0f9e2104
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-01 22:28:27 +0300

    - добавлено логирование

--- src/main/java/com/company/app/Application.java
+++ src/main/java/com/company/app/Application.java
@@ -1,12 +1,23 @@
 package com.company.app;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
+//@EnableCaching
+//@EnableAspectJAutoProxy
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner() {
+		return args -> log.debug("*******************  The app has been started.  *******************");
 	}
 }
--- src/main/java/com/company/app/exchangeRate/config/ExchangeRateSchedulerConfig.java
+++ src/main/java/com/company/app/exchangeRate/config/ExchangeRateSchedulerConfig.java
@@ -3,6 +3,7 @@ package com.company.app.exchangeRate.config;
 import com.company.app.core.main.api.NotificationService;
 import com.company.app.exchangeRate.ExchangeRateFacade;
 import com.company.app.exchangeRate.entity.ExchangeRate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
+@Slf4j
 public class ExchangeRateSchedulerConfig {
 
 	@Autowired
@@ -22,6 +24,10 @@ public class ExchangeRateSchedulerConfig {
 	@Scheduled(fixedDelayString = "${exchangeRate.timeout}")
 	public void writeExchange() {
 		ExchangeRate exchangeRate = exchangeRateFacade.extract();
-		notificationService.eventNotification(exchangeRate.getAliexpressExchangeRate() + " (курс али)");
+		String notification = exchangeRate.getAliexpressExchangeRate() + " (курс али)";
+		if (log.isDebugEnabled()) {
+			log.debug("Определён курс: [{}].", notification);
+		}
+		notificationService.eventNotification(notification);
 	}
 }

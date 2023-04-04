6e09768c299f31c835b2991e9d7d3579ecd05aff
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-04 14:16:31 +0300

    добавил рекурсионно рефлекционное выковыривание гуида

--- src/main/java/com/company/app/core/aop/logging/PerformanceLogAnnotation.java
+++ src/main/java/com/company/app/core/aop/logging/PerformanceLogAnnotation.java
@@ -6,9 +6,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Аннотация, для работы PerformanceLogAspect
+ * number - порядковый номер объекта в сигнатуре метода, начинается с 0.
+ * fieldName - поле объекта, содержащее UUID.
+ *
  * @author shibaev.aleksey 30.03.2023
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PerformanceLogAnnotation {
+
+	String number() default "";
+	String fieldName() default "";
 }
--- src/main/java/com/company/app/core/aop/logging/PerformanceLogAspect.java
+++ src/main/java/com/company/app/core/aop/logging/PerformanceLogAspect.java
@@ -3,22 +3,34 @@ package com.company.app.core.aop.logging;
 import com.google.common.base.Stopwatch;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Аспект, логирует разными способами производительность метода.
  * Метод должен быть помечен аннотацией @PerformanceLogAnnotation
  * <p>
- * UUID берется из первого параметра сигнатуры метода, либо генерируется рандомный.
+ * Способы получения GUID:
+ * 1.В аннотацию можно передать параметры. Пример @PerformanceLogAnnotation(number = "0", fieldName = "guid")
+ *  number - порядковый номер объекта в сигнатуре метода, начинается с 0.
+ *  fieldName - поле объекта, содержащее UUID.
+ * 2. Аннотация без параметров. Тогда гуид попытается вытащить из первого объекта в сигнатуре.
+ * 3. Если в п.1 или п.2 упали - сгенерируется рандомный гуид.
  * <p>
  * Способы логирования:
  * 1. Стандартный
@@ -46,48 +58,84 @@ public class PerformanceLogAspect {
 		if (log.isDebugEnabled()) {
 			Stopwatch stopwatch = Stopwatch.createStarted();
 			Signature signature = proceedingJoinPoint.getSignature();
-			String operationId = getOperationId(proceedingJoinPoint);
-			doLogBefore(operationId, signature);
+			String guid = getGuid(proceedingJoinPoint);
+			doLogBefore(guid, signature);
 
 			Object proceed = proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
 
 			stopwatch.stop();
-			doLogAfter(stopwatch, operationId, signature, proceed);
+			doLogAfter(stopwatch, guid, signature, proceed);
 			return proceed;
 		} else {
 			return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
 		}
 	}
 
-	private String getOperationId(ProceedingJoinPoint proceedingJoinPoint) {
+	private String getGuid(ProceedingJoinPoint proceedingJoinPoint) {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+
+		Signature signature = proceedingJoinPoint.getSignature();
 		try {
-			Object[] args = proceedingJoinPoint.getArgs();
-			UUID uuid = UUID.fromString(String.valueOf(args[0]));
-			return uuid.toString();
+			Method method = getMethod(signature);
+			PerformanceLogAnnotation annotation = method.getAnnotation(PerformanceLogAnnotation.class);
+			String number = annotation.number();
+
+			if (StringUtils.isNotEmpty(number)) {
+				int i = Integer.parseInt(annotation.number());
+				Object arg = proceedingJoinPoint.getArgs()[i];
+				Field field = recursiveFieldSearch(arg.getClass(), annotation.fieldName());
+				field.trySetAccessible();
+				Object object = field.get(arg);
+				UUID uuid = UUID.fromString(String.valueOf(object));
+				return uuid.toString();
+			} else {
+				Object[] args = proceedingJoinPoint.getArgs();
+				UUID uuid = UUID.fromString(String.valueOf(args[0]));
+				return uuid.toString();
+			}
 		} catch (Exception e) {
 			return UUID.randomUUID().toString();
+		} finally {
+			stopwatch.stop();
+			log.trace("выковыривание guid заняло [{}] ms.", stopwatch.elapsed(TimeUnit.MILLISECONDS));
 		}
 	}
 
-	private void doLogAfter(Stopwatch stopwatch, String operationId, Signature signature, Object proceed) {
+	@SneakyThrows
+	private <T> Field recursiveFieldSearch(Class<T> clazz, String fieldName) {
+		List<Field> fields = Arrays.stream(clazz.getDeclaredFields())
+				.filter(field -> field.getName().equals(fieldName))
+				.collect(Collectors.toList());
+		return CollectionUtils.isEmpty(fields) ? recursiveFieldSearch(clazz.getSuperclass(), fieldName) : fields.get(0);
+	}
+
+	private Method getMethod(Signature signature) {
+		for (Method method : signature.getDeclaringType().getDeclaredMethods()) {
+			if ((method.getName().equals(signature.getName()) && method.isAnnotationPresent(PerformanceLogAnnotation.class)))
+				return method;
+		}
+		throw new RuntimeException("этого никогда не случится");
+	}
+
+	private void doLogAfter(Stopwatch stopwatch, String guid, Signature signature, Object proceed) {
 		if (proceed instanceof Collection) {
-			doCollectionPerformanceLogging(stopwatch, operationId, signature, (Collection<?>) proceed);
+			doCollectionPerformanceLogging(stopwatch, guid, signature, (Collection<?>) proceed);
 		} else {
-			doDefaultPerformanceLogging(stopwatch, operationId, signature);
+			doDefaultPerformanceLogging(stopwatch, guid, signature);
 		}
 	}
 
-	private void doLogBefore(String operationId, Signature signature) {
+	private void doLogBefore(String guid, Signature signature) {
 		log.debug("[{}]: запущен {}.{}",
-				operationId,
+				guid,
 				signature.getDeclaringType().getName(),
 				signature.getName()
 		);
 	}
 
-	private void doCollectionPerformanceLogging(Stopwatch stopwatch, String operationId, Signature signature, Collection<?> proceed) {
+	private void doCollectionPerformanceLogging(Stopwatch stopwatch, String guid, Signature signature, Collection<?> proceed) {
 		log.debug("[{}]: за [{}] ms вернул [{}] шт. выполнен {}.{}",
-				operationId,
+				guid,
 				stopwatch.elapsed(TimeUnit.MILLISECONDS),
 				proceed.size(),
 				signature.getDeclaringType().getName(),
@@ -95,9 +143,9 @@ public class PerformanceLogAspect {
 		);
 	}
 
-	private void doDefaultPerformanceLogging(Stopwatch stopwatch, String operationId, Signature signature) {
+	private void doDefaultPerformanceLogging(Stopwatch stopwatch, String guid, Signature signature) {
 		log.debug("[{}]: за [{}] ms выполнен {}.{}",
-				operationId,
+				guid,
 				stopwatch.elapsed(TimeUnit.MILLISECONDS),
 				signature.getDeclaringType().getName(),
 				signature.getName()

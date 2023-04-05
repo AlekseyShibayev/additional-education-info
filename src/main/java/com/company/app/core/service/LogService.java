f5bbabd5a83661b9821e8098156472ca1fab81ed
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-06 00:27:51 +0300

    - удаляй за собой файл

--- src/main/java/com/company/app/core/service/LogService.java
+++ src/main/java/com/company/app/core/service/LogService.java
@@ -8,24 +8,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class LogService {
 
 	public static final String LOG_ZIP_FILE_NAME = "logs.zip";
-	public static final String LOG_PACKAGE_NAME = "logs";
+	public static final String PACKAGE_NAME = "logs";
 
 	@Autowired
 	DataExtractorService dataExtractorService;
 
 	@SneakyThrows
 	public byte[] getLogsAsZip() {
-		File file = new File(LOG_ZIP_FILE_NAME);
-		file.delete();
+		List<File> files = dataExtractorService.getFiles(PACKAGE_NAME);
+		byte[] bytes = getBytes(files);
+		FileUtils.forceDelete(new File(LOG_ZIP_FILE_NAME));
+		return bytes;
+	}
 
-		List<File> files = dataExtractorService.getFiles(LOG_PACKAGE_NAME);
-
+	private byte[] getBytes(List<File> files) throws IOException {
 		try (ZipFile zipFile = new ZipFile(LOG_ZIP_FILE_NAME)) {
 			zipFile.addFiles(files);
 			return FileUtils.readFileToByteArray(zipFile.getFile());

067a09621f007a455504ea873b8057f2324a6a77
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-06 00:38:42 +0300

    - sonar

--- src/main/java/com/company/app/telegram/repository/HistoryRepository.java
+++ src/main/java/com/company/app/telegram/repository/HistoryRepository.java
@@ -3,5 +3,10 @@ package com.company.app.telegram.repository;
 import com.company.app.telegram.entity.History;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface HistoryRepository extends CrudRepository<History, Long> {
+
+	@Override
+	List<History> findAll();
 }
--- src/test/java/com/company/app/application/ApplicationTest.java
+++ src/test/java/com/company/app/application/ApplicationTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public class ApplicationTest extends AbstractTest {
 
 	@LocalServerPort
@@ -31,18 +33,20 @@ public class ApplicationTest extends AbstractTest {
 		historyRepository.save(History.builder()
 				.message("0. Успешное сохранение в бд.")
 				.build());
-		Assert.assertEquals(Lists.newArrayList(historyRepository.findAll()).size(), 1);
+		Assert.assertEquals(1, Lists.newArrayList(historyRepository.findAll()).size());
 	}
 
 	@Test
 	public void notificationServiceSmokeTest() {
 		notificationService.eventNotification("1. Тестовое приложение поднялось.");
+		List<History> all = historyRepository.findAll();
+		Assert.assertNotNull(all);
 	}
 
 	@Test
 	public void controllerSmokeTest() {
 		String message = String.format("2. На порту %s успешно поднялось тестовое приложение.", port);
-		ResponseEntity<Object> forEntity = testRestTemplate.getForEntity(String.format("/telegram/say?message=%s", message), Object.class);
-		Assert.assertEquals(forEntity.getStatusCode(), HttpStatus.OK);
+		ResponseEntity<Object> entity = testRestTemplate.getForEntity(String.format("/telegram/say?message=%s", message), Object.class);
+		Assert.assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 }

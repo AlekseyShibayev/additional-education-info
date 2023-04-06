ccb7122ed270d3b19062f5a527374414fec808a0
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-06 20:38:49 +0300

    - ReflectionUnderworld - коммит перед удалением, это уже редкий случай и наркомания какая-то

--- src/main/java/com/company/app/core/aop/logging/performance/component/ReflectionUnderworld.java
+++ src/main/java/com/company/app/core/aop/logging/performance/component/ReflectionUnderworld.java
@@ -4,12 +4,56 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ReflectionUnderworld {
 
 	public Method getMethodIfOverloading(ProceedingJoinPoint proceedingJoinPoint, List<Method> collect) {
-		throw new RuntimeException("Такое возможно, если оверлоадинг.");
+		return collect.stream()
+				.filter(method -> isEquals(proceedingJoinPoint.getArgs(), method.getParameters()))
+				.findFirst()
+				.get();
+	}
+
+	private boolean isEquals(Object[] args, Parameter[] parameters) {
+		boolean result = true;
+		for (int i = 0; i < parameters.length; i++) {
+			Object realObject = args[i];
+			Parameter parameter = parameters[i];
+			Class<?> type = parameter.getType();
+
+			if (!isEqualsClass(realObject.getClass(), type)) {
+				return false;
+			}
+		}
+		return result;
+	}
+
+	private boolean isEqualsClass(Class<?> realObjectClass, Class<?> type) {
+		if (realObjectClass.equals(type)) {
+			return true;
+		} else {
+			if (!isNextParentObject(realObjectClass)) {
+				return isEqualsClass(realObjectClass.getSuperclass(), type);
+
+			} else {
+				Class<?>[] interfaces = realObjectClass.getInterfaces();
+				if (interfaces.length == 0) {
+					return false;
+				} else {
+					List<Class<?>> collect = Arrays.stream(interfaces)
+							.collect(Collectors.toList());
+					return true;
+				}
+			}
+		}
+	}
+
+	private boolean isNextParentObject(Class<?> clazz) {
+		return clazz.getSuperclass().equals(Object.class);
 	}
 }

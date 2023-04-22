106fcb528de3a1571b95302d919992117960acf7
shibaev.aleksey <shibaev.aleksey@otr.ru>
2023-04-22 14:22:00 +0300

    - хочу делать инициализацию чатов при подъеме приложения из json файла

--- src/main/java/com/company/app/telegram/component/impl/InitialChatRegistryImpl.java
+++ src/main/java/com/company/app/telegram/component/impl/InitialChatRegistryImpl.java
@@ -1,44 +1,32 @@
 package com.company.app.telegram.component.impl;
 
-import com.company.app.core.tool.api.DataExtractorTool;
+import com.company.app.core.tool.api.JsonSerializationTool;
 import com.company.app.telegram.component.api.InitialChatRegistry;
 import com.company.app.telegram.entity.Chat;
-import com.company.app.telegram.entity.UserInfo;
 import com.company.app.telegram.repository.ChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 public class InitialChatRegistryImpl implements InitialChatRegistry {
 
-	private static final String CHAT_PROPERTIES = "telegram/chat.properties";
+	@Value("classpath:telegram/init.json")
+	private Resource resource;
 
 	@Autowired
-	DataExtractorTool dataExtractorTool;
+	JsonSerializationTool<Chat> jsonSerializationTool;
 	@Autowired
 	ChatRepository chatRepository;
 
 	@PostConstruct
 	public void init() throws TelegramApiException {
-		Map<String, String> chats = dataExtractorTool.getProperties(CHAT_PROPERTIES);
-
-		List<Chat> list = chats.keySet().stream()
-				.map(chatId -> Chat.builder()
-						.chatId(Long.valueOf(chatId))
-						.enableNotifications(true)
-						.userInfo(UserInfo.builder()
-								.role("Owner")
-								.build())
-						.build()
-				)
-				.collect(Collectors.toList());
-
+		List<Chat> list = jsonSerializationTool.load(resource, Chat.class);
 		chatRepository.saveAll(list);
 	}
 }
